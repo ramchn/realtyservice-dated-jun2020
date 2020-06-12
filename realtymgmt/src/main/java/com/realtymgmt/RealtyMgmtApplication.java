fb33bfd090067c356ab9334c8f2e580cc57d8428
ramchn <ramamoorthyp@gmail.com>
2020-06-12 11:02:10 +0530

    comment added

--- realtymgmt/src/main/java/com/realtymgmt/RealtyMgmtApplication.java
+++ realtymgmt/src/main/java/com/realtymgmt/RealtyMgmtApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealtyMgmtApplication {
 
 	
-	
+	// this is spring boot app
 	public static void main(String[] args) {
 		SpringApplication.run(RealtyMgmtApplication.class, args);
 	}
