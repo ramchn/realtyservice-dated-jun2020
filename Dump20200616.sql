59629e35746e61a5c3b5f206d5f172290c35d982
ramchn <66200262+ramchn@users.noreply.github.com>
2020-06-17 19:51:02 +0530

    user access tables added

---
+++ Dump20200616.sql
@@ -0,0 +1,143 @@
+CREATE DATABASE  IF NOT EXISTS `RealtyDB` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `RealtyDB`;
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: RealtyDB
+-- ------------------------------------------------------
+-- Server version	5.7.30-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `access`
+--
+
+DROP TABLE IF EXISTS `access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `access` (
+  `access_id` int(11) NOT NULL AUTO_INCREMENT,
+  `authority` varchar(100) NOT NULL,
+  `user_email_address` varchar(255) NOT NULL,
+  PRIMARY KEY (`access_id`),
+  UNIQUE KEY `user_email_address` (`user_email_address`),
+  CONSTRAINT `fk_accessuser` FOREIGN KEY (`user_email_address`) REFERENCES `user` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_name` varchar(100) NOT NULL,
+  `services_offered` varchar(255) NOT NULL,
+  `user_email_address` varchar(100) NOT NULL,
+  PRIMARY KEY (`contact_id`),
+  KEY `fk_contactuser` (`user_email_address`),
+  CONSTRAINT `fk_contactuser` FOREIGN KEY (`user_email_address`) REFERENCES `user` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `owner`
+--
+
+DROP TABLE IF EXISTS `owner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `owner` (
+  `owner_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_name` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `user_email_address` varchar(100) NOT NULL,
+  PRIMARY KEY (`owner_id`),
+  KEY `fk_owneruser` (`user_email_address`),
+  CONSTRAINT `fk_owneruser` FOREIGN KEY (`user_email_address`) REFERENCES `user` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(45) DEFAULT NULL,
+  `task_description` varchar(255) DEFAULT NULL,
+  `task_status` varchar(45) DEFAULT NULL,
+  `task_createddate` datetime DEFAULT NULL,
+  `owner_owner_id` int(11) NOT NULL,
+  `tenant_tenant_id` int(11) NOT NULL,
+  `contact_contact_id` int(11) DEFAULT NULL,
+  `task_modifieddate` datetime DEFAULT NULL,
+  PRIMARY KEY (`task_id`) USING BTREE,
+  KEY `fk_taskowner` (`owner_owner_id`),
+  KEY `fk_tasktenant` (`tenant_tenant_id`),
+  KEY `fk_taskcontact` (`contact_contact_id`),
+  CONSTRAINT `fk_taskcontact` FOREIGN KEY (`contact_contact_id`) REFERENCES `contact` (`contact_id`),
+  CONSTRAINT `fk_taskowner` FOREIGN KEY (`owner_owner_id`) REFERENCES `owner` (`owner_id`),
+  CONSTRAINT `fk_tasktenant` FOREIGN KEY (`tenant_tenant_id`) REFERENCES `tenant` (`tenant_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tenant`
+--
+
+DROP TABLE IF EXISTS `tenant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tenant` (
+  `tenant_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tenant_name` varchar(45) NOT NULL,
+  `owner_owner_id` int(11) NOT NULL,
+  `user_email_address` varchar(100) NOT NULL,
+  PRIMARY KEY (`tenant_id`),
+  KEY `fk_tenantowner` (`owner_owner_id`),
+  KEY `fk_tenantuser` (`user_email_address`),
+  CONSTRAINT `fk_tenantowner` FOREIGN KEY (`owner_owner_id`) REFERENCES `owner` (`owner_id`),
+  CONSTRAINT `fk_tenantuser` FOREIGN KEY (`user_email_address`) REFERENCES `user` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `email_address` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`email_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-16 20:25:03
