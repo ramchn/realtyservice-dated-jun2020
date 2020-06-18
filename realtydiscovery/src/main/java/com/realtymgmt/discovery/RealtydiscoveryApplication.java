ed87d815d74861090c8a6d3834e303c5efa582be
ramchn <ramamoorthyp@gmail.com>
2020-06-18 20:08:21 +0530

    discovery server added

---
+++ realtydiscovery/src/main/java/com/realtymgmt/discovery/RealtydiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.realtymgmt.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtydiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtydiscoveryApplication.class, args);
+		
+	}
+
+}
