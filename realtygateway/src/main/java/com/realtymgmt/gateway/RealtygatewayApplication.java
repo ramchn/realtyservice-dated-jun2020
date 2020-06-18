6abf96815f581311cc257e875ca95f90bb504d09
ramchn <ramamoorthyp@gmail.com>
2020-06-18 21:46:39 +0530

    gateway server

---
+++ realtygateway/src/main/java/com/realtymgmt/gateway/RealtygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.realtymgmt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtygatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtygatewayApplication.class, args);
+	}
+
+}
